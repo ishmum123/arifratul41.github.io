c004706f95db5ae3255888880f18a5916d91eedb
Ariful <arifratul41@gmail.com>
2021-04-26 15:29:30 +0600

    adds cypress

---
+++ cypress/integration/sample_spec.js
@@ -0,0 +1,23 @@
+describe('My First Test', () => {
+  it('clicking "type" shows the right headings', () => {
+    // cy.visit('https://example.cypress.io')
+    cy.visit('index.html')
+
+    cy.get('#distance')
+        .type("100")
+    cy.get("#distanceUnits")
+        .select('km')
+    cy.get('#fuelUsed')
+        .type("10")
+    cy.get("#fuelUnits")
+        .select('litres')
+    cy.get('#priceOfFuel')
+        .type("100")
+    cy.get("#fuelPriceUnits")
+        .select('litres')
+    cy.contains("Calculate")
+        .click()
+    cy.get("#USFuelEfficiency")
+        .should('include.text', '23.52')
+})
+})
